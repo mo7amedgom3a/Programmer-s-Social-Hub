879e424dd24cdcc9ea9f3af97cc4cfe99c30dc7f
mo7amedgom3a <mo7amed.gom3a.7moda@gmail.com>
2024-10-20 21:07:18 +0300

    create register layout page

---
+++ Frontend/pages/register.tsx
@@ -0,0 +1,13 @@
+"use client"
+import React from "react";
+import '../app/globals.css';
+import { RegisterComponent } from "@/app/components/Register";
+import exp from "constants";
+
+const Register = () => {
+    return (
+        
+            <RegisterComponent />
+    )
+}
+export default Register;
