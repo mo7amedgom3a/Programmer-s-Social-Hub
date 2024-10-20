55f4d1a56b7d1cc6f066cfad1a6c2b44f63add82
mo7amedgom3a <mo7amed.gom3a.7moda@gmail.com>
2024-10-20 21:07:43 +0300

    create search layout page

---
+++ Frontend/pages/search.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import SearchPage from "../app/components/search"; // Assuming SearchPage is in the components folder
+
+const Search = () => {
+  return <SearchPage />;
+};
+
+export default Search;
