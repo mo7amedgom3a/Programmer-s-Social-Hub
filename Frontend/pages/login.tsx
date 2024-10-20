dc9db3c67aa67b4231e1a7bfee4dbbcaebe46cff
mo7amedgom3a <mo7amed.gom3a.7moda@gmail.com>
2024-10-20 21:06:32 +0300

    create login page routing

---
+++ Frontend/pages/login.tsx
@@ -0,0 +1,14 @@
+"use client"
+import React from "react";
+import { Input } from "../app/components/ui/input";
+import LoginComponent from "../app/components/Login";
+import { Button } from "../app/components/ui/button";
+import '../app/globals.css';
+const Login = () => {
+    return (
+       
+            <LoginComponent />
+
+    )
+    }
+export default Login;
